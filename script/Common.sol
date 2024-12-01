006d86e972fc3ddd55dab951707314223cbe4695
Hunter King <hking6776@gmail.com>
2024-12-01 18:17:28 +0100

    refactor: add deplyment script

--- script/Common.sol
+++ script/Common.sol
@@ -1,11 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.23;
 
-// import {IERC20} from '@oz/token/ERC20/IERC20.sol';
-import {IBuildersManager} from 'contracts/BuildersManager.sol';
+import {TransparentUpgradeableProxy} from '@oz/proxy/transparent/TransparentUpgradeableProxy.sol';
+import {BuildersManager, IBuildersManager} from 'contracts/BuildersManager.sol';
 import {Script} from 'forge-std/Script.sol';
 // solhint-disable-next-line
 import 'script/Registry.sol';
+
+address constant EAS = address(0x420);
+address constant BUILDERS_DOLLAR = address(0x421);
+
+struct DeploymentParams {
+  address token; // BuildersDollar token address
+  address eas; // Ethereum Attestation Service address
+  string name; // Contract name for EIP712
+  string version; // Contract version for EIP712
+  IBuildersManager.BuilderManagerSettings settings; // Settings struct
+}
 
 /**
  * @title Common Contract
@@ -16,17 +27,48 @@ import 'script/Registry.sol';
 contract Common is Script {
   IBuildersManager public builderManager;
 
+  // @notice Deployer address will be the owner of the proxy
+  address public deployer;
+
   /// @notice Deployment parameters for each chain
-  // mapping(uint256 _chainId => DeploymentParams _params) internal _deploymentParams;
+  mapping(uint256 _chainId => DeploymentParams _settings) internal _deploymentParams;
 
   function setUp() public virtual {
     // Optimism
-    // _deploymentParams[10] = DeploymentParams('Hello, Optimism!', IERC20(OPTIMISM_DAI));
+    _deploymentParams[10] = DeploymentParams({
+      token: BUILDERS_DOLLAR, // Replace with actual BuildersDollar address
+      eas: EAS, // Replace with actual EAS address
+      name: 'BuildersManager',
+      version: '1',
+      settings: IBuildersManager.BuilderManagerSettings({
+        cycleLength: 7 days,
+        lastClaimedTimestamp: uint64(block.timestamp),
+        currentSeasonExpiry: uint64(block.timestamp + 180 days),
+        seasonDuration: 365 days,
+        minVouches: 3,
+        optimismFoundationAttesters: new address[](0) // Replace with actual attesters
+      })
+    });
   }
 
   function _deployContracts() internal {
-    // DeploymentParams memory _params = _deploymentParams[block.chainid];
+    DeploymentParams memory _settings = _deploymentParams[block.chainid];
 
-    // builderManager = new BuildersManager(_params.greeting, _params.token);
+    address token = _settings.token;
+    address eas = _settings.eas;
+    string memory name = _settings.name;
+    string memory version = _settings.version;
+    IBuildersManager.BuilderManagerSettings memory settings = _settings.settings;
+
+    address _implementation = address(new BuildersManager());
+    builderManager = BuildersManager(
+      address(
+        new TransparentUpgradeableProxy(
+          _implementation,
+          deployer,
+          abi.encodeWithSelector(IBuildersManager.initialize.selector, token, eas, name, version, settings)
+        )
+      )
+    );
   }
 }
--- script/Deploy.sol
+++ script/Deploy.sol
@@ -5,10 +5,11 @@ import {Common} from 'script/Common.sol';
 
 contract Deploy is Common {
   function run() public {
+    uint256 _deployerPk = uint256(vm.envBytes32('OPTIMISM_DEPLOYER_PK'));
+    deployer = vm.addr(_deployerPk);
+
     vm.startBroadcast();
-
     _deployContracts();
-
     vm.stopBroadcast();
   }
 }
