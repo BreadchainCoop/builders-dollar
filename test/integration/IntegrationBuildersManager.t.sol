48dd3024cd3b8b3f274ac766a2bf93cf071ffb82
Hunter King <hking6776@gmail.com>
2025-02-04 20:54:11 -0500

    fix: add filler integration test

--- test/integration/IntegrationBuildersManager.t.sol
+++ test/integration/IntegrationBuildersManager.t.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.27;
 import {IntegrationBase} from 'test/integration/IntegrationBase.sol';
 
 contract IntegrationBuildersManager is IntegrationBase {
-  function test_NullTest() public view {
+  function test_NullTest() public pure {
     assertTrue(true);
   }
 }
